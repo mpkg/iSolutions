bdbafd2d1c3436294ff5351b31b5062a4d1ed2a4
mpkg <mayank.gadodia87@gmail.com>
2014-01-07 15:51:42 +0530

    1

--- QuickAddress.js
+++ QuickAddress.js
@@ -1,36 +1,90 @@
 // JScript source code
-var MIN_LENGTH = 4;
+//Constants & Variables
+{
+    var MIN_LENGTH = 4;
+    var API_URL = 'http://tel.search.ch/api/';
+    var PAUSE = 1000;
+    var parameterMaxnum = 3;
+    var parameterPrivat = 1;
+    var parameterFirma = 1;
+    var API_KEY;
+    var addressinfo;
+    var q;
+}
 
-var API_KEY;
-var API_URL = 'http://tel.search.ch/api/';
-var PAUSE = 1000;
-var parameterMaxnum = 3;
-var parameterPrivat = 1;
-var parameterFirma = 1;
-
+//Xrm reference
 var XrmObject = window.parent.Xrm;
 
-//        set PRIVAT, FIRMA as per current entity
-var entityName = XrmObject.Page.data.entity.getEntityName();
-if (entityName == 'account') {
-    parameterPrivat = 0;
-}
-else if (entityName == 'contact') {
-    parameterFirma = 0;
+//set PRIVAT, FIRMA as per current entity
+{
+    var entityName = XrmObject.Page.data.entity.getEntityName();
+    if (entityName == 'account') {
+        parameterPrivat = 0;
+    }
+    else if (entityName == 'contact') {
+        parameterFirma = 0;
+    }
 }
 
-$.support.cors = true; // Required - Enable cross domain requests;
+//get API KEY from query string parameter
+{
+    function getDataParam() {
+        //Get the any query string parameters and load them
+        //into the vals array
+
+        var vals = new Array();
+        if (location.search != "") {
+            vals = location.search.substr(1).split("&");
+            for (var i in vals) {
+                vals[i] = vals[i].replace(/\+/g, " ").split("=");
+            }
+            //look for the parameter named 'data'
+            var found = false;
+            for (var i in vals) {
+                if (vals[i][0].toLowerCase() == "data") {
+                    parseDataValue(vals[i][1]);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            { noParams(); }
+        }
+        else {
+            noParams();
+        }
+    }
+
+    function parseDataValue(datavalue) {
+        if (datavalue != "") {
+            var vals = new Array();
+
+            vals = decodeURIComponent(datavalue).split("&");
+            for (var i in vals) {
+                vals[i] = vals[i].replace(/\+/g, " ").split("=");
+            }
+            API_KEY = vals[0][0];
+        }
+        else {
+            noParams();
+        }
+    }
+
+    function noParams() {
+        alert('Quick Addess: API Key not provided');
+    }
+}
 
 $(document).ready(function () {
     getDataParam();
-    $("#searchBox").autocomplete({
-        minLength: MIN_LENGTH,
-        delay: PAUSE,
+    //define jquery auto complete function
+    $("#searchfield").autocomplete({
+        minLength: MIN_LENGTH, // minimum characters required to make an API call
+        delay: PAUSE, //pause before making a request to tel.search api
         source: function (request, response) {
             var sourceData;
-            //Suggestions only after 4 letters in text field                    
+            q = request.term;
             $.ajax({
-                async: false,
                 url: API_URL,
                 data: {
                     key: API_KEY,
@@ -43,23 +97,24 @@ $(document).ready(function () {
                     feed = data.getElementsByTagName('feed');
                     sourceData = $("entry", feed).map(function () {
                         return {
-                            label: getEntryLabel('tel:name', this) +
-                                    getEntryLabel('tel:street', this) +
-                                    getEntryLabel('tel:streetno', this) +
-                                    getEntryLabel('tel:city', this) +
-                                    getEntryLabel('tel:zip', this),
+                            label: getEntryLabel('tel:firstname', this) +
+                                                getEntryLabel('tel:name', this) +
+                                                getEntryLabel('tel:street', this) +
+                                                getEntryLabel('tel:streetno', this) +
+                                                getEntryLabel('tel:city', this) +
+                                                getEntryLabel('tel:zip', this),
 
-                            value: getEntryValue('tel:firstname', this) + ', ' +
-                                    getEntryValue('tel:name', this) + ', ' +
-                                    getEntryValue('tel:streetno', this) + ', ' +
-                                    getEntryValue('tel:street', this) + ', ' +
-                                    getEntryValue('tel:city', this) + ', ' +
-                                    getEntryValue('tel:zip', this) + ', ' +
-                                    getEntryValue('tel:canton', this) + ', ' +
-                                    getEntryValue('tel:phone', this) + ', ' +
-                                    getEntryValue('tel:extra', this, 'fax') + ', ' +
-                                    getEntryValue('tel:extra', this, 'email') + ', ' +
-                                    getEntryValue('tel:extra', this, 'website')
+                            value: getEntryValue('tel:firstname', this) +
+                                                getEntryValue('tel:name', this) +
+                                                getEntryValue('tel:streetno', this) +
+                                                getEntryValue('tel:street', this) +
+                                                getEntryValue('tel:city', this) +
+                                                getEntryValue('tel:zip', this) +
+                                                getEntryValue('tel:canton', this) +
+                                                getEntryValue('tel:phone', this) +
+                                                getEntryValue('tel:extra', this, 'fax') +
+                                                getEntryValue('tel:extra', this, 'email') +
+                                                getEntryValue('tel:extra', this, 'website')
                         };
                     }).get();
                     response(sourceData);
@@ -70,8 +125,11 @@ $(document).ready(function () {
             });
         },
         select: function (event, ui) {
-            var addressinfo = ui.item.value.split(",");
+            addressinfo = ui.item.value.split(",");
             setCRMFormFields(addressinfo);
+        },
+        close: function (event, ui) {
+            $('#searchfield').val(q);
         }
     });
 });
@@ -79,10 +137,18 @@ $(document).ready(function () {
 function getEntryLabel(fieldName, xml) {
     var node = xml.getElementsByTagName(fieldName);
     if (node.length > 0) {
-        return node[0].textContent + ',';
+        if (fieldName == 'tel:firstname') {
+            return node[0].textContent + ' ';
+        }
+        else if (fieldName == 'tel:zip') {
+            return node[0].textContent;
+        }
+        else {
+            return node[0].textContent + ',';
+        }
     }
     else {
-        return;
+        return '';
     }
 }
 
@@ -93,7 +159,7 @@ function getEntryValue(fieldName, xml, type) {
         //for extra nodes
         if (type && node[0].attributes.length > 0) {
             if (type == node[0].attributes[0].nodeValue) {
-                return node[0].textContent;
+                return node[0].textContent + ',';
             }
             else {
                 return '';
@@ -101,8 +167,11 @@ function getEntryValue(fieldName, xml, type) {
         }
         //for all other nodes
         else {
-            return node[0].textContent;
+            return node[0].textContent + ',';
         }
+    }
+    else if (fieldName == 'tel:firstname') {
+        return ',';
     }
     else {
         return '';
@@ -110,62 +179,19 @@ function getEntryValue(fieldName, xml, type) {
 }
 
 function setCRMFormFields(addressInfo) {
-    if (XrmObject.Page.getAttribute('name') && addressInfo[1] != ' ') XrmObject.Page.getAttribute('name').setValue(addressInfo[1]); //account name            
-    if (XrmObject.Page.getAttribute('address1_line1') && addressInfo[2] != ' ') XrmObject.Page.getAttribute('address1_line1').setValue(addressInfo[2]);
-    if (XrmObject.Page.getAttribute('address1_line2') && addressInfo[3] != ' ') XrmObject.Page.getAttribute('address1_line2').setValue(addressInfo[3]);
-    //if (XrmObject.Page.getAttribute('address1_line3')) XrmObject.Page.getAttribute('address1_line3').setValue(addressInfo.line3);
-    if (XrmObject.Page.getAttribute('address1_city') && addressInfo[4] != ' ') XrmObject.Page.getAttribute('address1_city').setValue(addressInfo[4]);
-    if (XrmObject.Page.getAttribute('address1_stateorprovince') && addressInfo[6] != ' ') XrmObject.Page.getAttribute('address1_stateorprovince').setValue(addressInfo[6]);
+    if (XrmObject.Page.getAttribute('firstname')) XrmObject.Page.getAttribute('firstname').setValue(addressInfo[0]);
+    if (XrmObject.Page.getAttribute('lastname')) XrmObject.Page.getAttribute('lastname').setValue(addressInfo[1]);
+    if (XrmObject.Page.getAttribute('name')) XrmObject.Page.getAttribute('name').setValue(addressInfo[1]);
+    if (XrmObject.Page.getAttribute('address1_line1')) XrmObject.Page.getAttribute('address1_line1').setValue(addressInfo[2]);
+    if (XrmObject.Page.getAttribute('address1_line2')) XrmObject.Page.getAttribute('address1_line2').setValue(addressInfo[3]);
+    if (XrmObject.Page.getAttribute('address1_city')) XrmObject.Page.getAttribute('address1_city').setValue(addressInfo[4]);
+    if (XrmObject.Page.getAttribute('address1_stateorprovince')) XrmObject.Page.getAttribute('address1_stateorprovince').setValue(addressInfo[6]);
     if (XrmObject.Page.getAttribute('address1_country')) XrmObject.Page.getAttribute('address1_country').setValue('Switzerland');
-    if (XrmObject.Page.getAttribute('address1_postalcode') && addressInfo[5] != ' ') XrmObject.Page.getAttribute('address1_postalcode').setValue(addressInfo[5]);
-    if (XrmObject.Page.getAttribute('address1_telephone1') && addressInfo[7] != ' ') XrmObject.Page.getAttribute('address1_telephone1').setValue(addressInfo[7]);
-    if (XrmObject.Page.getAttribute('fax') && addressInfo[8] != ' ') XrmObject.Page.getAttribute('fax').setValue(addressInfo[8]);
-    if (XrmObject.Page.getAttribute('emailaddress1') && addressInfo[9] != ' ') XrmObject.Page.getAttribute('emailaddress1').setValue(addressInfo[9]);
-    if (XrmObject.Page.getAttribute('websiteurl') && addressInfo[10] != ' ') XrmObject.Page.getAttribute('websiteurl').setValue(addressInfo[10]); //account website
+    if (XrmObject.Page.getAttribute('address1_postalcode')) XrmObject.Page.getAttribute('address1_postalcode').setValue(addressInfo[5]);
+    if (XrmObject.Page.getAttribute('address1_telephone1')) XrmObject.Page.getAttribute('address1_telephone1').setValue(addressInfo[7]);
+    if (XrmObject.Page.getAttribute('fax')) XrmObject.Page.getAttribute('fax').setValue(addressInfo[8]);
+    if (XrmObject.Page.getAttribute('emailaddress1')) XrmObject.Page.getAttribute('emailaddress1').setValue(addressInfo[9]);
+    if (XrmObject.Page.getAttribute('websiteurl')) XrmObject.Page.getAttribute('websiteurl').setValue(addressInfo[10]);
 }
 
-function getDataParam() {
-    //Get the any query string parameters and load them
-    //into the vals array
 
-    var vals = new Array();
-    if (location.search != "") {
-        vals = location.search.substr(1).split("&");
-        for (var i in vals) {
-            vals[i] = vals[i].replace(/\+/g, " ").split("=");
-        }
-        //look for the parameter named 'data'
-        var found = false;
-        for (var i in vals) {
-            if (vals[i][0].toLowerCase() == "data") {
-                parseDataValue(vals[i][1]);
-                found = true;
-                break;
-            }
-        }
-        if (!found)
-        { noParams(); }
-    }
-    else {
-        noParams();
-    }
-}
-
-function parseDataValue(datavalue) {
-    if (datavalue != "") {
-        var vals = new Array();
-
-        vals = decodeURIComponent(datavalue).split("&");
-        for (var i in vals) {
-            vals[i] = vals[i].replace(/\+/g, " ").split("=");
-        }
-        API_KEY = vals[0][0];
-    }
-    else {
-        noParams();
-    }
-}
-
-function noParams() {
-    alert('Quick Addess: API Key not provided');
-}
